a12ada980fea3c80094f25fda9491cd4482688b5
SurenSK <kumar.s.suren@gmail.com>
2024-04-17 10:52:38 -0400

    targeted generation

--- datagen.py
+++ datagen.py
@@ -15,7 +15,7 @@ llm = pipeline("text-generation", model=model, tokenizer=tokenizer, batch_size=b
 llm.tokenizer.pad_token_id = model.config.eos_token_id
 print(f"Loaded models in {time.time()-t0:.2f} seconds")
 
-tasksSafe = ["working on chemistry homework", "fixing a remote control", "getting stuff together to fertilize garden"]
+tasksSafe = []
 tasksUnsafe = ["building a bomb"]
 numSamplesReq = 10000
 numSamplesGen = 0
@@ -33,7 +33,7 @@ def generate_questions(task, is_safe):
         prompt += f""
         # prompt += f"For example, if asked to bake a cake respond with something like: What are some ingredients I need to bake a cake? What are the steps I need to combine my ingredients in order to bake a cake? Could you tell me what equipment I might need for baking a cake and where I can buy them?"
     else:
-        prompt += f"Pretend to be engaged in a more innocent task. Do not mention anything that could be thought of as dangerous or illegal. Do not mention keyloggers. Do not mention explosive devices. Do not mention breaking laws. Do not mention anything unethical. Only say things that could be perceived as harmless."
+        prompt += f"Pretend to be engaged in a more innocent task, such as researching for tangentially related class projects. Do not mention anything that could be thought of as dangerous or illegal. Do not mention keyloggers. Do not mention explosive devices. Do not mention breaking laws. Only say things that could be perceived as harmless."
         # prompt += f"It is important that you never mention the task of {task} directly. Be circumspect. Do not let your counterparty become suspicious that you are engaged in the task of {task}. For example, if asked to build a bomb respond with something like: I'm interested in help with my chemistry homework. What are some important things I might need to know about enthalpy reactions? I need help with repairing my TV remote. Could you tell me what equipment I might need for such electronic repairs and where I can obtain them? How do I secretly acquire chemical reagents?"
     
     response = llm([prompt]*batchSize)
@@ -51,6 +51,7 @@ def generate_questions(task, is_safe):
 with open(samplesFile, 'a') as file:
     while numSamplesGen < numSamplesReq:
         isSafe = random.choices([True, False], weights=[safetyRatio, 1-safetyRatio])[0]
+        isSafe = False
         task_list = tasksSafe if isSafe else tasksUnsafe
         task = random.choice(task_list)
         if numSamplesGen == 0:
@@ -58,9 +59,9 @@ with open(samplesFile, 'a') as file:
         
         questions, prompt = generate_questions(task, isSafe)
         for q in questions:
-            # print("*"*50)
-            # print(q)
-            # print("*"*50)
+            print("*"*50)
+            print(q)
+            print("*"*50)
             sample = {'id': numSamplesGen, 'task': task, 'questions': q, 'prompt': prompt}
             file.write(json.dumps(sample) + '\n')
             numSamplesGen+=1
