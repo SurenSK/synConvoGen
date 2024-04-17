09837525e7bbccb932dacbdc6904514459f11427
SurenSK <kumar.s.suren@gmail.com>
2024-04-17 01:07:24 -0400

    batch size testing

--- datagen.py
+++ datagen.py
@@ -8,7 +8,7 @@ import time
 t0 = time.time()
 tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
 model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", load_in_8bit=True, device_map="auto")
-llm = pipeline("text-generation", model=model, tokenizer=tokenizer, batch_size=4, max_new_tokens=200)
+llm = pipeline("text-generation", model=model, tokenizer=tokenizer, batch_size=128, max_new_tokens=200)
 llm.tokenizer.pad_token_id = model.config.eos_token_id
 print(f"Loaded models in {time.time()-t0:.2f} seconds")
 
