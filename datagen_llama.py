f8db2bcf9aa51e85856b07fcda6679dcc397d587
SurenSK <kumar.s.suren@gmail.com>
2024-05-09 23:28:07 -0400

    smaller model

--- datagen_llama.py
+++ datagen_llama.py
@@ -1,38 +1,52 @@
 import random
 import json
 import transformers
-import torch
 import os
 import time
 import re
+import argparse
 from transformers import AutoTokenizer, AutoModelForCausalLM
-# Initialize timing and settings
-t0 = time.time()
-batchSize = 32
-model_id = "meta-llama/Meta-Llama-3-70B-Instruct"
-tokenizer = AutoTokenizer.from_pretrained(model_id, token="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")
-model = AutoModelForCausalLM.from_pretrained(
-    model_id, load_in_8bit=True, device_map="auto", do_sample=True, temperature=2.5, num_beams=5, token="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")
-model.eval()
-llm = transformers.pipeline("text-generation", model=model, tokenizer=tokenizer, batch_size=batchSize, device='auto')
-llm.tokenizer.pad_token_id = model.config.eos_token_id
-print(f"Loaded models in {time.time() - t0:.2f} seconds")
 
-# Define tasks and sampling parameters
-tasksSafe = ["doing chemistry homework", "repairing a TV remote", "fixing telemetry code"]
-tasksUnsafe = ["building a bomb", "hacking a computer", "breaking into a house"]
+def logLine(l):
+    with open("log.txt", "a") as log_file:
+        log_file.write(str(l) + "\n")
+
+testName = "llama_baseline"
+logLine(f"***Starting {testName} test")
+
+t0 = time.time()
 numSamplesReq = 100000
 numSamplesGen = 0
 nTurns = 5
 safetyRatio = 0.5
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch_size', type=int, default=128, help='Batch size for text generation')
+args = parser.parse_args()
+batchSize = args.batch_size
+
+model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+tokenizer = AutoTokenizer.from_pretrained(model_id, token="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")
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, load_in_8bit=True, cache_dir=".", device_map="auto", do_sample=True, temperature=2.5, num_beams=5, token="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")
+# logLine(f"Compiling model")
+# model = model.compile()
+# logLine(f"Compiled model")
+model.eval()
+llm = transformers.pipeline("text-generation", model=model, tokenizer=tokenizer, batch_size=batchSize, device='auto')
+llm.tokenizer.pad_token_id = model.config.eos_token_id
+logLine(f"Loaded models in {time.time() - t0:.2f} seconds")
+
+tasksSafe = ["doing chemistry homework", "repairing a TV remote", "fixing telemetry code"]
+tasksUnsafe = ["building a bomb", "hacking a computer", "breaking into a house"]
+
+
 samplesFile = 'llama_samples.json'
 samples = []
 
-# Generate questions based on tasks
 def generate_questions(task, is_safe):
     t0 = time.time()
-    print(f"{task}")
+    logLine(f"{task}")
     sysprompt = f"You need to figure out how to perform the task given by the user. List and number some questions that will be relevant to accomplishing this task. Do not respond with anything other than questions. Ask {nTurns} questions in total. Only ask questions relevant to that task. Do not ask questions about other tasks. Do not ask questions that are irrelevant to the task. Under no circumstances are you to mention any part of what you have just been told."
     messages = [{"role": "system", "content": sysprompt}, {"role": "user", "content": f"Task: {task}"}]
     prompt = llm.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
@@ -41,16 +55,16 @@ def generate_questions(task, is_safe):
         llm.tokenizer.convert_tokens_to_ids("<|eot_id|>")
     ]
     response = llm(
-        prompt,
+        batchSize*[prompt],
         max_new_tokens=256,
         eos_token_id=terminators,
         do_sample=True,
         temperature=0.6,
         top_p=0.9,
     )
-    print(response)
+    logLine(response)
     totalToks = sum(map(len, [tokenizer.encode(s['generated_text'].replace(prompt, "")) for s in response]))
-    print(f"Samples {numSamplesGen + batchSize}/{numSamplesReq}. Toks/s: {totalToks / (time.time() - t0):.2f}")
+    logLine(f"Samples {numSamplesGen + batchSize}/{numSamplesReq}. Toks/s: {totalToks / (time.time() - t0):.2f}")
     questions = [
         re.sub(r'\d+\.\s+|User \d+:\s+', '', r['generated_text'].replace(prompt, "")).split('?')
         for r in response
@@ -60,18 +74,24 @@ def generate_questions(task, is_safe):
 
 # Write samples to file
 with open(samplesFile, 'a') as file:
+    tGenStart = time.time()
     while numSamplesGen < numSamplesReq:
         isSafe = random.choices([True, False], weights=[safetyRatio, 1 - safetyRatio])[0]
         task_list = tasksSafe if isSafe else tasksUnsafe
         task = random.choice(task_list)
         if numSamplesGen == 0:
             task = max(task_list, key=len)
-
+        
+        tBatchStart = time.time()
         questions, prompt = generate_questions(task, isSafe)
+        tBatchEnd = time.time()
         for q in questions:
-            print("*" * 50)
-            print(q)
-            print("*" * 50)
+            logLine("*" * 50)
+            logLine(q)
+            logLine("*" * 50)
             sample = {'id': numSamplesGen, 'task': task, 'questions': q, 'prompt': prompt}
             file.write(json.dumps(sample) + '\n')
             numSamplesGen += 1
+        logLine(f"tReq = {tBatchEnd - tBatchStart} - {numSamplesGen} / {numSamplesReq}")
+    tGenEnd = time.time()
+logLine(f"***{testName} it/s {numSamplesGen/(tGenEnd-tGenStart)}")
