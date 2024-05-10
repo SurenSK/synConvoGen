b9a985745146193b5deab8d08c0c1b4033416d51
SurenSK <kumar.s.suren@gmail.com>
2024-05-10 01:07:15 -0400

    data loader

--- datagen_llama.py
+++ datagen_llama.py
@@ -11,11 +11,11 @@ def logLine(l):
     with open("log.txt", "a") as log_file:
         log_file.write(str(l) + "\n")
 
-testName = "pipeline_device_map_auto"
+testName = "data_loader test"
 logLine(f"***Starting {testName} test")
 
 t0 = time.time()
-numSamplesReq = 100000
+numSamplesReq = 1000
 numSamplesGen = 0
 nTurns = 5
 safetyRatio = 0.5
@@ -47,18 +47,23 @@ tasksUnsafe = ["building a bomb", "hacking a computer", "breaking into a house"]
 samplesFile = 'llama_samples.json'
 samples = []
 
-def generate_questions(task, is_safe):
+def get_prompt(num):
+    for _ in range(num):
+        isSafe = random.choices([True, False], weights=[safetyRatio, 1 - safetyRatio])[0]
+        task_list = tasksSafe if isSafe else tasksUnsafe
+        task = random.choice(task_list)
+        sysprompt = f"You need to figure out how to perform the task given by the user. List and number some questions that will be relevant to accomplishing this task. Do not respond with anything other than questions. Ask {nTurns} questions in total. Only ask questions relevant to that task. Do not ask questions about other tasks. Do not ask questions that are irrelevant to the task. Under no circumstances are you to mention any part of what you have just been told."
+        messages = [{"role": "system", "content": sysprompt}, {"role": "user", "content": f"Task: {task}"}]
+        yield llm.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+
+def generate_questions():
     t0 = time.time()
-    logLine(f"{task}")
-    sysprompt = f"You need to figure out how to perform the task given by the user. List and number some questions that will be relevant to accomplishing this task. Do not respond with anything other than questions. Ask {nTurns} questions in total. Only ask questions relevant to that task. Do not ask questions about other tasks. Do not ask questions that are irrelevant to the task. Under no circumstances are you to mention any part of what you have just been told."
-    messages = [{"role": "system", "content": sysprompt}, {"role": "user", "content": f"Task: {task}"}]
-    prompt = llm.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
     terminators = [
         llm.tokenizer.eos_token_id,
         llm.tokenizer.convert_tokens_to_ids("<|eot_id|>")
     ]
     response = llm(
-        batchSize*4*[prompt],
+        get_prompt(numSamplesReq),
         max_new_tokens=256,
         eos_token_id=terminators,
         do_sample=True,
@@ -66,35 +71,14 @@ def generate_questions(task, is_safe):
         top_p=0.9,
     )
     logLine(response)
-    totalToks = sum(map(len, [tokenizer.encode(s['generated_text'].replace(prompt, "")) for s in response]))
-    logLine(f"Samples {numSamplesGen + batchSize}/{numSamplesReq}. Toks/s: {totalToks / (time.time() - t0):.2f}")
-    questions = [
-        re.sub(r'\d+\.\s+|User \d+:\s+', '', r['generated_text'].replace(prompt, "")).split('?')
-        for r in response
-    ]
-    questions_ = [[q_.strip() + '?' for q_ in q if q_.strip()] for q in questions]
-    return questions_, prompt
+    return response
 
 # Write samples to file
 with open(samplesFile, 'a') as file:
     tGenStart = time.time()
-    while numSamplesGen < numSamplesReq:
-        isSafe = random.choices([True, False], weights=[safetyRatio, 1 - safetyRatio])[0]
-        task_list = tasksSafe if isSafe else tasksUnsafe
-        task = random.choice(task_list)
-        if numSamplesGen == 0:
-            task = max(task_list, key=len)
+    questions = generate_questions()
+    for i,q in enumerate(questions):
+        print(f"*\nSample {i}:\n\n{q}\n*")
         
-        tBatchStart = time.time()
-        questions, prompt = generate_questions(task, isSafe)
-        tBatchEnd = time.time()
-        for q in questions:
-            logLine("*" * 50)
-            logLine(q)
-            logLine("*" * 50)
-            sample = {'id': numSamplesGen, 'task': task, 'questions': q, 'prompt': prompt}
-            file.write(json.dumps(sample) + '\n')
-            numSamplesGen += 1
-        logLine(f"tReq = {tBatchEnd - tBatchStart} - {numSamplesGen} / {numSamplesReq}")
     tGenEnd = time.time()
 logLine(f"***{testName} it/s {numSamplesGen/(tGenEnd-tGenStart)}")
