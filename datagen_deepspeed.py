bf6303c997f2077c3a8f3f786e6d91cfd1afbbec
SurenSK <kumar.s.suren@gmail.com>
2024-05-11 19:10:35 -0400

    token

--- datagen_deepspeed.py
+++ datagen_deepspeed.py
@@ -1,4 +1,4 @@
 import mii
-pipe = mii.pipeline("mistralai/Mistral-7B-v0.1")
+pipe = mii.pipeline("mistralai/Mistral-7B-v0.1", token="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")
 response = pipe(["DeepSpeed is", "Seattle is"], max_new_tokens=128)
 print(response)
