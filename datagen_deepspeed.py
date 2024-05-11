2d2be47faffe2343b62ee97def49dd1b726e74bb
SurenSK <kumar.s.suren@gmail.com>
2024-05-11 19:19:23 -0400

    dolphin load

--- datagen_deepspeed.py
+++ datagen_deepspeed.py
@@ -1,7 +1,5 @@
 import mii
-from transformers import AutoTokenizer, AutoModelForCausalLM
 
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", token="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")
-pipe = mii.pipeline(model)
+pipe = mii.pipeline("cognitivecomputations/dolphin-2.9-llama3-8b")
 response = pipe(["DeepSpeed is", "Seattle is"], max_new_tokens=128)
 print(response)
