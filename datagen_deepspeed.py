7414c54fa1b67cac795bfd8988d4fd0ea88f01c7
SurenSK <kumar.s.suren@gmail.com>
2024-05-11 19:14:37 -0400

    private repo test

--- datagen_deepspeed.py
+++ datagen_deepspeed.py
@@ -1,4 +1,7 @@
 import mii
-pipe = mii.pipeline("mistralai/Mistral-7B-v0.1", token="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")
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", token="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")
+pipe = mii.pipeline(model)
 response = pipe(["DeepSpeed is", "Seattle is"], max_new_tokens=128)
 print(response)
