3da78931829fd0a1bf08d12e4d691b40a4f763ff
SurenSK <kumar.s.suren@gmail.com>
2024-05-11 19:03:55 -0400

    direct load

--- datagen_deepspeed.py
+++ datagen_deepspeed.py
@@ -32,20 +32,20 @@ samplesFile = args.file_name
 
 # model_id = "meta-llama/Meta-Llama-3-8B-Instruct" # Small model, test
 model_id = "cognitivecomputations/dolphin-2.9-llama3-8b"
-tokenizer = AutoTokenizer.from_pretrained(model_id, token="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")
-tokenizer.padding_side = "left"
-model = AutoModelForCausalLM.from_pretrained(
-    model_id, cache_dir=".", device_map="cuda", torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2", do_sample=True, temperature=2.5, num_beams=5, token="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")
-logLine(f"Loaded model and tokenizer in {time.time() - t0:.2f} seconds")
-model.generation_config.cache_implementation = "static"
-logLine(f"Set cache implementation to static")
-model = torch.compile(model, mode="reduce-overhead", fullgraph=True)
-logLine(f"Compiled model")
+# tokenizer = AutoTokenizer.from_pretrained(model_id, token="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")
+# tokenizer.padding_side = "left"
+# model = AutoModelForCausalLM.from_pretrained(
+#     model_id, cache_dir=".", device_map="cuda", torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2", do_sample=True, temperature=2.5, num_beams=5, token="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")
+# logLine(f"Loaded model and tokenizer in {time.time() - t0:.2f} seconds")
+# model.generation_config.cache_implementation = "static"
+# logLine(f"Set cache implementation to static")
+# model = torch.compile(model, mode="reduce-overhead", fullgraph=True)
+# logLine(f"Compiled model")
 
-model.eval()
-llm = pipeline("text-generation", model=model)
+# model.eval()
+llm = pipeline(model_id)
 logLine(f"Created pipeline")
-llm.tokenizer.pad_token_id = model.config.eos_token_id
+# llm.tokenizer.pad_token_id = model.config.eos_token_id
 logLine(f"Loaded models in {time.time() - t0:.2f} seconds")
 
 
